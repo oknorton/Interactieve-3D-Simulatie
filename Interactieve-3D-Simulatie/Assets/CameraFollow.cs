eee149f3aaf1ebe4e961961bf83004a6320d9258
Oliver Norton <o.k.norton21@gmail.com>
2024-03-02 18:28:06 +0100

    Top down movement, rotation and camera tracking

---
+++ Interactieve-3D-Simulatie/Assets/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3f;
+    public Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            Vector3 targetPosition = target.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
